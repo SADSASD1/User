65832941d1e2779ff5484bf0b835e501093e5761
SADSASD1 <aleqsanyan1996@.ru>
2018-10-29 17:29:08 +0400

    User project

---
+++ Userapp/src/sample/Controller.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField First_Name;
+
+    @FXML
+    private TextField Last_Name;
+
+    @FXML
+    private TextField EmailField;
+
+    @FXML
+    private PasswordField PasswordField;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
