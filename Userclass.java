0abafb6af0bfd3afb097bebd6350bc6a87d6d8fd
SADSASD1 <aleqsanyan1996@.ru>
2018-11-02 17:26:02 +0400

    new userclass object

---
+++ Userclass.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Userclass {
+
+    Userclass user= new Userclass(){
+        JTextField fs=new JTextField("Firstname");
+        JTextField ls=new JTextField("Lastname");
+        JTextField em=new JTextField("Email");
+        JPasswordField ps=new JPasswordField();
+
+    };
+
+}
